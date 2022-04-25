67fd6b8401a2dee48df0de400463e1ed0e678e9d
BlueBall_PL <69431322+BlueBall33@users.noreply.github.com>
2022-04-25 14:49:12 +0200

    Update mine.py

--- mine.py
+++ mine.py
@@ -1,21 +1,15 @@
 import os
 import random
-
+from config.py as config import *
 import nextcord as discord
 from nextcord.ext import commands, tasks
 
-#======# CONFIG #======#
 
-Tocken = ""
-Prefix = ""
-statusy = [
-    f'{Prefix}pomoc',
-    'Redy'
-]
+
 
 #=======================#
 
-client = commands.Bot(command_prefix=Prefix, case_insensitive=True)
+client = commands.Bot(command_prefix=config.Prefix, case_insensitive=True)
 client.remove_command("help")
 client.members = True
 
@@ -29,7 +23,7 @@ for filename in os.listdir('./cogs'):
 
 @tasks.loop(seconds=10)
 async def status():
-    await client.change_presence(activity=discord.Game(name=statusy[random.randint(0, len(statusy))-1]))
+    await client.change_presence(activity=discord.Game(name=config.Statusy[random.randint(0, len(statusy))-1]))
 
 
 
